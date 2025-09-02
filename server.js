5595693a91ee2e2cd2c88cffa5df626ab6f63648
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-02 18:35:32 +0200

    Updated MongoDB connection message for clarity.

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/kham-pha-viec-lam-tu-xa';
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connection successful'))
-  .catch(err => console.error('MongoDB connection error: ', err));
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB: ', err));
 
 // Home page
 app.get('/', (req, res) => {
